68eb9f404d651c26f76b58079f7e016b09d73113
reverendus <reverendus@invalid>
2017-12-04 21:17:22 +0100

    Getting the chart working

--- market_maker_stats/oasis_market_maker_stats.py
+++ market_maker_stats/oasis_market_maker_stats.py
@@ -16,11 +16,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
+import datetime
 import sys
 from functools import reduce
-from pprint import pformat
-from typing import List
+from typing import List, Optional
 
+import matplotlib.dates as md
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
 from web3 import Web3, HTTPProvider
 
 from pymaker import Address
@@ -29,12 +32,37 @@ from pymaker.oasis import SimpleMarket, Order, LogMake, LogTake, LogKill
 
 
 class State:
-    def __init__(self, timestamp: int, order_book: List[Order]):
+    def __init__(self, timestamp: int, order_book: List[Order], sai_address: Address, weth_address: Address):
         self.timestamp = timestamp
         self.order_book = order_book
+        self.sai_address = sai_address
+        self.weth_address = weth_address
 
-    def __repr__(self):
-        return pformat(vars(self))
+    def closest_sell_price(self) -> Optional[Wad]:
+        return min(self.sell_prices(), default=None)
+
+    def furthest_sell_price(self) -> Optional[Wad]:
+        return max(self.sell_prices(), default=None)
+
+    def closest_buy_price(self) -> Optional[Wad]:
+        return max(self.buy_prices(), default=None)
+
+    def furthest_buy_price(self) -> Optional[Wad]:
+        return min(self.buy_prices(), default=None)
+
+    def sell_orders(self) -> List[Order]:
+        return list(filter(lambda order: order.buy_which_token == self.sai_address and
+                                         order.sell_which_token == self.weth_address, self.order_book))
+
+    def sell_prices(self) -> List[Wad]:
+        return list(map(lambda order: order.buy_to_sell_price, self.sell_orders()))
+
+    def buy_orders(self) -> List[Order]:
+        return list(filter(lambda order: order.buy_which_token == self.weth_address and
+                                         order.sell_which_token == self.sai_address, self.order_book))
+
+    def buy_prices(self) -> List[Wad]:
+        return list(map(lambda order: order.sell_to_buy_price, self.buy_orders()))
 
 
 class OasisMarketMakerStats:
@@ -45,11 +73,15 @@ class OasisMarketMakerStats:
         parser.add_argument("--rpc-host", help="JSON-RPC host (default: `localhost')", default="localhost", type=str)
         parser.add_argument("--rpc-port", help="JSON-RPC port (default: `8545')", default=8545, type=int)
         parser.add_argument("--oasis-address", help="Ethereum address of the OasisDEX contract", required=True, type=str)
+        parser.add_argument("--sai-address", help="Ethereum address of the SAI token", required=True, type=str)
+        parser.add_argument("--weth-address", help="Ethereum address of the WETH token", required=True, type=str)
         parser.add_argument("--market-maker-address", help="Ethereum account of the market maker to analyze", required=True, type=str)
         parser.add_argument("--past-blocks", help="Number of past blocks to analyze", required=True, type=int)
         self.arguments = parser.parse_args(args)
 
         self.web3 = kwargs['web3'] if 'web3' in kwargs else Web3(HTTPProvider(endpoint_uri=f"http://{self.arguments.rpc_host}:{self.arguments.rpc_port}"))
+        self.sai_address = Address(self.arguments.sai_address)
+        self.weth_address = Address(self.arguments.weth_address)
         self.market_maker_address = Address(self.arguments.market_maker_address)
         self.otc = SimpleMarket(web3=self.web3, address=Address(self.arguments.oasis_address))
 
@@ -77,12 +109,33 @@ class OasisMarketMakerStats:
             for log_kill in filter(lambda log_kill: log_kill.timestamp == timestamp, past_kill):
                 order_book = self.apply_kill(order_book, log_kill)
 
-            return states + [State(timestamp=timestamp, order_book=order_book)]
+            return states + [State(timestamp=timestamp,
+                                   order_book=order_book,
+                                   sai_address=self.sai_address,
+                                   weth_address=self.weth_address)]
 
         event_timestamps = sorted(set(map(lambda event: event.timestamp, past_make + past_take + past_kill)))
         states = reduce(reduce_func, event_timestamps, [])
-        print(states)
+        self.draw(states)
 
+    def draw(self, states: List[State]):
+        plt.subplots_adjust(bottom=0.2)
+        plt.xticks( rotation=25 )
+        ax=plt.gca()
+        xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+        ax.xaxis.set_major_formatter(xfmt)
+
+        timestamps = list(map(lambda state: date2num(datetime.datetime.fromtimestamp(state.timestamp)), states))
+        closest_sell_prices = list(map(lambda state: state.closest_sell_price(), states))
+        furthest_sell_prices = list(map(lambda state: state.furthest_sell_price(), states))
+        closest_buy_prices = list(map(lambda state: state.closest_buy_price(), states))
+        furthest_buy_prices = list(map(lambda state: state.furthest_buy_price(), states))
+
+        plt.plot_date(timestamps, furthest_sell_prices, 'b:')
+        plt.plot_date(timestamps, closest_sell_prices, 'b--')
+        plt.plot_date(timestamps, closest_buy_prices, 'g--')
+        plt.plot_date(timestamps, furthest_buy_prices, 'g:')
+        plt.show()
 
     def apply_make(self, order_book: List[Order], log_make: LogMake) -> List[Order]:
         return order_book + [Order(self.otc,
