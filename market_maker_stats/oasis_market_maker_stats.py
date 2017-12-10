dee673deacff1c85a7f776642e3a3ba32d2f0ff3
reverendus <reverendus@invalid>
2017-12-10 21:59:26 +0100

    Updated `pymaker` dependency, fixed changed property names

--- market_maker_stats/oasis_market_maker_stats.py
+++ market_maker_stats/oasis_market_maker_stats.py
@@ -54,15 +54,15 @@ class State:
         return min(self.buy_prices(), default=None)
 
     def sell_orders(self) -> List[Order]:
-        return list(filter(lambda order: order.buy_which_token == self.sai_address and
-                                         order.sell_which_token == self.weth_address, self.order_book))
+        return list(filter(lambda order: order.buy_token == self.sai_address and
+                                         order.pay_token == self.weth_address, self.order_book))
 
     def sell_prices(self) -> List[Wad]:
         return list(map(lambda order: order.buy_to_sell_price, self.sell_orders()))
 
     def buy_orders(self) -> List[Order]:
-        return list(filter(lambda order: order.buy_which_token == self.weth_address and
-                                         order.sell_which_token == self.sai_address, self.order_book))
+        return list(filter(lambda order: order.buy_token == self.weth_address and
+                                         order.pay_token == self.sai_address, self.order_book))
 
     def buy_prices(self) -> List[Wad]:
         return list(map(lambda order: order.sell_to_buy_price, self.buy_orders()))
@@ -102,7 +102,7 @@ class OasisMarketMakerStats:
             # apply all LogMake events having this timestamp
             for log_make in filter(lambda log_make: log_make.timestamp == timestamp, past_make):
                 order_book = self.apply_make(order_book, log_make)
-                order_book = list(filter(lambda order: order.owner == self.market_maker_address, order_book))
+                order_book = list(filter(lambda order: order.maker == self.market_maker_address, order_book))
 
             # apply all LogTake events having this timestamp
             for log_take in filter(lambda log_take: log_take.timestamp == timestamp, past_take):
@@ -210,31 +210,31 @@ class OasisMarketMakerStats:
     def apply_make(self, order_book: List[Order], log_make: LogMake) -> List[Order]:
         return order_book + [Order(self.otc,
                                    order_id=log_make.order_id,
-                                   sell_how_much=log_make.pay_amount,
-                                   sell_which_token=log_make.pay_token,
-                                   buy_how_much=log_make.buy_amount,
-                                   buy_which_token=log_make.buy_token,
-                                   owner=log_make.maker,
+                                   pay_amount=log_make.pay_amount,
+                                   pay_token=log_make.pay_token,
+                                   buy_amount=log_make.buy_amount,
+                                   buy_token=log_make.buy_token,
+                                   maker=log_make.maker,
                                    timestamp=log_make.timestamp)]
 
     def apply_take(self, order_book: List[Order], log_take: LogTake):
         this_order = next(filter(lambda order: order.order_id == log_take.order_id, order_book), None)
 
         if this_order is not None:
-            assert this_order.sell_which_token == log_take.pay_token
-            assert this_order.buy_which_token == log_take.buy_token
+            assert this_order.pay_token == log_take.pay_token
+            assert this_order.buy_token == log_take.buy_token
 
             remaining_orders = list(filter(lambda order: order.order_id != log_take.order_id, order_book))
             this_order = Order(self.otc,
                                order_id=this_order.order_id,
-                               sell_how_much=this_order.sell_how_much - log_take.take_amount,
-                               sell_which_token=this_order.sell_which_token,
-                               buy_how_much=this_order.buy_how_much - log_take.give_amount,
-                               buy_which_token=this_order.buy_which_token,
-                               owner=this_order.owner,
+                               pay_amount=this_order.pay_amount - log_take.take_amount,
+                               pay_token=this_order.pay_token,
+                               buy_amount=this_order.buy_amount - log_take.give_amount,
+                               buy_token=this_order.buy_token,
+                               maker=this_order.maker,
                                timestamp=this_order.timestamp)
 
-            if this_order.sell_how_much > Wad(0) and this_order.buy_how_much > Wad(0):
+            if this_order.pay_amount > Wad(0) and this_order.buy_amount > Wad(0):
                 return remaining_orders + [this_order]
             else:
                 return remaining_orders
