3b0091f43076886273ba5efac46694cfb2432874
reverendus <reverendus@invalid>
2018-03-20 13:23:47 -0300

    `trade-client` dependency added

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PYTHONPATH=$PYTHONPATH:./lib/pymaker:./lib/pyexchange py.test --cov=market_maker_stats --cov-report=term --cov-append tests/
+PYTHONPATH=$PYTHONPATH:./lib/pymaker:./lib/pyexchange:./lib/trade-client py.test --cov=market_maker_stats --cov-report=term --cov-append tests/
